a6eea7c6ec5e62bd7eef1cbe518c675d32058886
kuzmak161-creator <kuzmak161@gmail.com>
2026-07-16 12:49:48 +0300

    Force structured repository deploy

--- update-repo.sh
+++ update-repo.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 echo "[*] Generating Packages index for stable/main..."
-# Сканируем pool, но пути в индексе будут считаться от корня репозитория
-dpkg-scanpackages pool/main /dev/null > dists/stable/main/binary-aarch64/Packages
+
+# Сканируем pool, находясь в корне репозитория
+dpkg-scanpackages pool /dev/null > dists/stable/main/binary-aarch64/Packages
 
 echo "[*] Compressing Packages index to gzip..."
 gzip -c dists/stable/main/binary-aarch64/Packages > dists/stable/main/binary-aarch64/Packages.gz
