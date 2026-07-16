6cf436544ab9d22c4406a1fec0dbf240e5febc43
kuzmak161-creator <kuzmak161@gmail.com>
2026-07-16 17:42:28 +0300

    added abc-pascal-tui and textual

--- update-repo.sh
+++ update-repo.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 echo "[*] Generating Packages index for stable/main..."
 
-# Сканируем pool, передавая /dev/null вместо файла оверрайдов
+# Создаем папки для обеих архитектур, если их нет
+mkdir -p dists/stable/main/binary-aarch64
+mkdir -p dists/stable/main/binary-all
+
+# Сканируем пакеты и генерируем индексы для aarch64
 dpkg-scanpackages pool /dev/null > dists/stable/main/binary-aarch64/Packages
 
+# Дублируем/линкуем индексы для архитектуры 'all', так как наши пакеты "Architecture: all"
+cp dists/stable/main/binary-aarch64/Packages dists/stable/main/binary-all/Packages
+
 echo "[*] Compressing Packages index to gzip..."
-gzip -c dists/stable/main/binary-aarch64/Packages > dists/stable/main/binary-aarch64/Packages.gz
+gzip -cf dists/stable/main/binary-aarch64/Packages > dists/stable/main/binary-aarch64/Packages.gz
+gzip -cf dists/stable/main/binary-all/Packages > dists/stable/main/binary-all/Packages.gz
 
 echo "[+] Done!"
