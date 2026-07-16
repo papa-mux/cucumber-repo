daac2db86d63054563a1cb59615e47c72cbdf395
kuzmak161-creator <kuzmak161@gmail.com>
2026-07-16 12:42:43 +0300

    Switch to structured repository layout (dists/pool)

--- update-repo.sh
+++ update-repo.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-echo "[*] Generating Packages index..."
-dpkg-scanpackages . /dev/null > Packages
+echo "[*] Generating Packages index for stable/main..."
+# Сканируем pool, но пути в индексе будут считаться от корня репозитория
+dpkg-scanpackages pool/main /dev/null > dists/stable/main/binary-aarch64/Packages
+
 echo "[*] Compressing Packages index to gzip..."
-gzip -c Packages > Packages.gz
+gzip -c dists/stable/main/binary-aarch64/Packages > dists/stable/main/binary-aarch64/Packages.gz
+
 echo "[+] Done!"
